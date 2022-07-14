d5802719c91f258be7500dc44bf363f2e0dc3379
NizarAffess <nizaraffess@gmail.com>
2022-07-14 10:53:23 +0100

    combine all apps

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
 import './App.css';
 import Bookmarks from './Components/Bookmarks/Bookmarks';
-// import Todo from './Components/Todo/Todo';
-// import Time from './Components/Time/Time';
-// import Greeting from './Components/Greeting/Greeting';
-// import Weather from './Components/Weather/Weather';
-// import Crypto from './Components/Crypto/Crypto';
+import Todo from './Components/Todo/Todo';
+import Crypto from './Components/Crypto/Crypto';
+import Weather from './Components/Weather/Weather';
+import Greeting from './Components/Greeting/Greeting';
+import Time from './Components/Time/Time';
 
 function App() {
   return (
-    <div className="App">
-      {/* <Time /> */}
-      {/* <Greeting /> */}
-      {/* <Weather /> */}
-      {/* <Crypto /> */}
-      {/* <Todo /> */}
-      <Bookmarks />
+    <div className="app-wrapper">
+      <div className="App">
+        <div className="item1">
+          <Bookmarks />
+        </div>
+        <div className="item2">
+          <Time />
+        </div>
+        <div className="item3">
+          <Greeting />
+        </div>
+        <div className="item4">
+          <Weather />
+        </div>
+        <div className="item5">
+          <Todo />
+        </div>
+        <div className="item6">
+          <Crypto />
+        </div>
+      </div>
     </div>
   );
 }
